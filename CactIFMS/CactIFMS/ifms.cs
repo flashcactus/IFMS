3618227b3d9a51330e369fc1539ff89120b09db1
Ivan Uvarov <flashcactus@ya.ru>
2015-01-26 04:51:18 +0300

    Added lotsa info: relative positions, distances, the like. Doesn't work :P

--- CactIFMS/CactIFMS/ifms.cs
+++ CactIFMS/CactIFMS/ifms.cs
@@ -6,11 +6,32 @@ using UnityEngine;
 
 namespace CactIFMS
 {
+	public class Differ {//numerical differentiator
+		public float step = 1f;
+		public float ppdist = 0f;
+		public float pdist = 0f;
+
+		public Differ(float tstep){
+			step = tstep;
+		}
+
+		public float update(float dist, float tstep){
+			step = tstep;
+			float left, right;
+			left = (pdist - ppdist) / (step * 2);
+			right = (dist - pdist) / (step * 2);
+			ppdist = pdist; 
+			pdist = dist;
+			return right + left;
+		}
+	}
+
 	[KSPAddon(KSPAddon.Startup.Flight , false)]
 	public class CactIFMS : MonoBehaviour
 	{
 		private Rect _windowRect = new Rect(0f, 0f, 400f, 400f);
-
+		private Vector3 vescom, vesfgwp, vestrans, kscpos;
+		private Differ vel1, vel2, vel3;
 		void Start()
 		{
 			// initially set window in middle of screen
@@ -26,15 +47,35 @@ namespace CactIFMS
 			_windowRect =
 				KSPUtil.ClampRectToScreen(GUILayout.Window(GetInstanceID(), _windowRect, DrawWindow, "IFMS"));
 		}
+		
+		private string stringifyVec(Vector3 v){
+			return string.Format("<{0:###.##} {1:###.##} {2:###.##}>", v.x, v.y, v.z);
+		}
 
 		void DrawWindow(int winid)
 		{
-			GUILayout.BeginHorizontal();
-			GUILayout.Label("Since startup: " + Time.realtimeSinceStartup.ToString("F2"));
-			GUILayout.EndHorizontal();
+			GUILayout.BeginVertical();
+			GUILayout.Label("KSC pos: " + stringifyVec(kscpos));
+			GUILayout.Label("Vessel pos (CoM): " + stringifyVec(vescom));
+			GUILayout.Label("rel.ves.pos (CoM): " + stringifyVec(vescom-kscpos));
+			GUILayout.Label("rel.ves.dst (CoM): " + (vescom-kscpos).magnitude.ToString("0.00"));
+			GUILayout.Label("rel.ves.dst (CoM): " + vel1.update((vescom-kscpos).magnitude, TimeWarp.fixedDeltaTime).ToString("0.00"));
+			GUILayout.Label("Vessel pos (GWP): " + stringifyVec(vesfgwp));
+			GUILayout.Label("rel.ves.pos (GWP): " + stringifyVec(vesfgwp-kscpos));
+			GUILayout.Label("rel.ves.dst (GWP): " + (vesfgwp-kscpos).magnitude.ToString("0.00"));
+			GUILayout.Label("rel.ves.dst (GWP): " + vel2.update((vesfgwp-kscpos).magnitude, TimeWarp.fixedDeltaTime).ToString("0.00"));
+			GUILayout.Label("Vessel pos (transform): " + stringifyVec(vestrans));
+			GUILayout.Label("rel.ves.pos (transform): " + stringifyVec(vestrans-kscpos));
+			GUILayout.Label("rel.ves.dst (transform): " + (vestrans-kscpos).magnitude.ToString("0.00"));
+			GUILayout.Label("rel.ves.dst (transform): " + vel3.update((vestrans-kscpos).magnitude, TimeWarp.fixedDeltaTime).ToString("0.00"));
+
+			GUILayout.EndVertical();
 
 			GUI.DragWindow(); // make window draggable
 		}
+
+
+
 	//code from TE's sample mod
 		private float lastUpdate = 0.0f;
 		private float lastFixedUpdate = 0.0f;
@@ -67,6 +108,11 @@ namespace CactIFMS
 				Debug.Log("CactIFMS [" + this.GetInstanceID().ToString("X")
 					+ "][" + Time.time.ToString("0.0000") + "]: FixedUpdate");
 			}
+			vescom = FlightGlobals.ActiveVessel.findWorldCenterOfMass();
+			vesfgwp = FlightGlobals.ActiveVessel.GetWorldPos3D();
+			vestrans = FlightGlobals.ActiveVessel.transform.position;
+			kscpos = GameObject.Find("KSC").transform.position;
+			//TimeWarp.fixedDeltaTime
 		}
 		/*
 * Called when the game is leaving the scene (or exiting). Perform any clean up work here.
