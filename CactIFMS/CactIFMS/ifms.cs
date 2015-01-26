0338fb5ee9b73a4b9f188a6eff982da69a8fa43a
Ivan Uvarov <flashcactus@ya.ru>
2015-01-26 03:15:46 +0300

    added some code from TE's tutorial plugin for some debug output

--- CactIFMS/CactIFMS/ifms.cs
+++ CactIFMS/CactIFMS/ifms.cs
@@ -16,6 +16,8 @@ namespace CactIFMS
 			// initially set window in middle of screen
 			_windowRect.x = Screen.width * 0.5f - _windowRect.width * 0.5f;
 			_windowRect.y = Screen.height * 0.5f - _windowRect.height * 0.5f;
+			Debug.Log("CactIFMS [" + this.GetInstanceID().ToString("X")
+				+ "][" + Time.time.ToString("0.0000") + "]: Start");
 
 		}
 
@@ -32,6 +34,47 @@ namespace CactIFMS
 			GUILayout.EndHorizontal();
 
 			GUI.DragWindow(); // make window draggable
+		}
+	//code from TE's sample mod
+		private float lastUpdate = 0.0f;
+		private float lastFixedUpdate = 0.0f;
+		private float logInterval = 5.0f;
+		void Awake()
+		{
+			Debug.Log("CactIFMS [" + this.GetInstanceID().ToString("X")
+				+ "][" + Time.time.ToString("0.0000") + "]: Awake: " + this.name);
+		}
+		/*
+* Called every frame
+*/
+		void Update()
+		{
+			if ((Time.time - lastUpdate) > logInterval)
+			{
+				lastUpdate = Time.time;
+				Debug.Log("CactIFMS [" + this.GetInstanceID().ToString("X")
+					+ "][" + Time.time.ToString("0.0000") + "]: Update");
+			}
+		}
+		/*
+* Called at a fixed time interval determined by the physics time step.
+*/
+		void FixedUpdate()
+		{
+			if ((Time.time - lastFixedUpdate) > logInterval)
+			{
+				lastFixedUpdate = Time.time;
+				Debug.Log("CactIFMS [" + this.GetInstanceID().ToString("X")
+					+ "][" + Time.time.ToString("0.0000") + "]: FixedUpdate");
+			}
+		}
+		/*
+* Called when the game is leaving the scene (or exiting). Perform any clean up work here.
+*/
+		void OnDestroy()
+		{
+			Debug.Log("CactIFMS [" + this.GetInstanceID().ToString("X")
+				+ "][" + Time.time.ToString("0.0000") + "]: OnDestroy");
 		}
 	}
 }
